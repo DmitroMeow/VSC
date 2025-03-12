103759fb2912efe2cadc85e774504845db08dbce
DmitroMeow <r36339584@gmail.com>
2025-03-12 21:20:14 +0200

    Designing, just wait.

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Home</title>
-            </head>
-            <body>
-                <h1>Welcome to the home page</h1>
-            </body>
-        </html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: rgb(14, 16, 31);
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1 {
+            color: rgb(58, 102, 198);
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome, I just designing it. Come back later.</h1>
+</body>
+</html>
+
         `);
 });
 
