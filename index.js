ff196f51b2c9a0619f2367538d3ce25027a632b2
DmitroMeow <r36339584@gmail.com>
2025-03-29 12:21:03 +0200

    Testing

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the root path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'browser.html'));
 });
+
 app.get('/lol', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'lol.html'));
 });
+
+app.post('/post', (req, res) => {
+    res.send('POST request received!');
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
