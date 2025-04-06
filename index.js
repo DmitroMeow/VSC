ff04aec0aac022bdc600ea49da0a91d7967a5d28
DmitroMeow <r36339584@gmail.com>
2025-04-06 16:46:11 +0300

    Added weather (UA), other in dev

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'browser.html'));
+let weatherData = { current: {} };
+// Function to fetch weather data
+async function fetchWeather() {
+  try {
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/current.json?key=c2fca2c38b884246bc2104149252303&q=Ukraine&aqi=no",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching weather: ${response.statusText}`);
+    }
+    weatherData = await response.json(); // Parse the JSON response
+    console.log(weatherData.current);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
+
+// Fetch weather data every minute
+fetchWeather(); // Initial fetch
+setInterval(fetchWeather, 1000 * 60); // Fetch every 60 seconds
+
+app.post("/weather", (req, res) => {
+  res.send(weatherData.current);
+  console.log(weatherData.current);
 });
 
-app.get('/lol', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'lol.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "browser.html"));
 });
 
-app.post('/post', (req, res) => {
-    res.send('POST request received!');
-});
+// app.get('/signin', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'signin.html'));
+// });
+
+// app.get('/login', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'login.html'));
+// });
+
+// app.get('/account', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'account.html'));
+// });
+
+// app.post('/login', (req, res) => {
+//     const { email, password } = req.body;
+//     res.send("Proccessing");
+// });
+
+// app.post('/signin', (req, res) => {
+//     const { email, username, password } = req.body;
+//     res.send("Proccessing");
+// });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
